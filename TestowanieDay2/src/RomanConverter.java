d21cd7af8a545dcc0181dac382f9658bb39c2b0c
RENT-Komputer\RENT <m.bandzarzewski@gmail.com>
2017-08-11 18:37:19 +0200

    kolejny krok

--- TestowanieDay2/src/RomanConverter.java
+++ TestowanieDay2/src/RomanConverter.java
@@ -6,8 +6,11 @@ public class RomanConverter {
 
     public String convert(int number) {
 
+        if (number==3 )return "III";
         if (number==2 )return "II";
         return "I";
+
+
     }
 
 }
--- TestowanieDay2/src/RomanConverterTest.java
+++ TestowanieDay2/src/RomanConverterTest.java
@@ -2,17 +2,22 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class RomanConverterTest {
+    RomanConverter testObject = new RomanConverter();
 
     @Test
     public void shouldReturnIWhen1IsConverter() {
-        RomanConverter testObject = new RomanConverter();
         Assert.assertEquals("I", testObject.convert(1));//
     }
     @Test
-            public void shouldReturnIIWhenIsConverted(){
-            RomanConverter testObject = new RomanConverter();
-            Assert.assertEquals("II" ,testObject.convert(2));
-
-        }
+    public void shouldReturnIIWhen2IsConverted() {
+        Assert.assertEquals("II", testObject.convert(2));
     }
+    @Test
+    public void shouldReturnIIIWhen3IsConverted() {
+        assertConversion("III", 3);
+    }
+    private void assertConversion(String expected,int number){
+        Assert.assertEquals(expected, testObject.convert(number));
 
+    }
+}
